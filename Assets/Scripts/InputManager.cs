2409ee48061e262c2c9bbd193a4e46f90e50d4a9
DoloressD <sarah.omar.a.a@gmail.com>
2022-07-10 19:57:56 -0700

    First Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask mouseInputMask;
+    public GameObject buildingPrefab;
+    public int cellsize = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    public void GetInput()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray.origin,ray.direction, out hit, Mathf.Infinity, mouseInputMask))
+            {
+                Vector3 position = hit.point - transform.position;
+                Debug.Log(CalculateGridPosition(position));
+                CreateBuilding(CalculateGridPosition(position));
+            }
+        }
+    }
+
+    public Vector3 CalculateGridPosition (Vector3 inputPosition)
+	{
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellsize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellsize);
+
+        return new Vector3(x * cellsize, 0, z * cellsize);
+    }
+
+    private void CreateBuilding(Vector3 gridPosition)
+	{
+        Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/SnapToGround.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class SnapToGround : MonoBehaviour
+{
+    [MenuItem("Custom/Snap to Object Below %g")]
+    public static void Ground()
+    {
+        if (Selection.transforms.Length > 0)
+        {
+            Vector3[] positions = new Vector3[Selection.transforms.Length];
+
+            foreach (var transform in Selection.transforms)
+            {
+                Bounds tBounds = transform.GetComponent<Renderer>().bounds;
+
+                var hits = Physics.RaycastAll(transform.position + Vector3.up, Vector3.down, 10f);
+
+                if (hits.Length > 0)
+                {
+                    positions.Initialize();
+
+                    foreach (var hit in hits)
+                    {
+                        if (hit.collider.gameObject == transform.gameObject)
+                            continue;
+
+                        float yPos = hit.point.y + (transform.gameObject.transform.localScale.y / 2);
+                        positions[positions.Length - 1] = new Vector3(transform.gameObject.transform.position.x, yPos, hit.point.z);
+                    }
+
+                    if (positions.Length > 0)
+                    {
+                        positions = positions.OrderBy(v => v.y).ToArray<Vector3>();
+                        transform.position = positions[positions.Length - 1];
+                        tBounds = transform.GetComponent<Renderer>().bounds;
+
+                        float yBounds = tBounds.min.y + transform.gameObject.transform.localScale.y / 2;
+                        float tempValue = transform.position.y - yBounds;
+
+                        if (yBounds < transform.position.y)
+                        {
+                            float gap = transform.position.y - yBounds;
+                            transform.position = new Vector3(transform.position.x, transform.position.y + gap, transform.position.z);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
